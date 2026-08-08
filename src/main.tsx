92aa389fca4ad45c95e7c25abfc012bd651cd2b6
bwichienkur <120682941+bwichienkur@users.noreply.github.com>
2026-08-08 18:08:35 -0400

    Fix wall selection crash in src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {AppErrorBoundary} from './components/ui/AppErrorBoundary';
 import './styles.css';
 import './planner.css';
 import './loading.css';
@@ -8,4 +9,4 @@ import './advanced.css';
 import './openings.css';
 import './catalog.css';
 import './mobile.css';
-ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><App /></React.StrictMode>);
+ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><AppErrorBoundary><App /></AppErrorBoundary></React.StrictMode>);
